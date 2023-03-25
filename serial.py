ea912250d311ef9ead25a3f64c0a12c99561c4f0
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-25 10:20:41 -0400

    Implement solution for serial.py

--- serial.py
+++ serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,16 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start):
+        """Initialize the start number and setup a sequence number for the generator"""
+        self.start = start
+        self.next_number = self.start - 1
+
+    def generate(self):
+        """Generate a number based on the current sequence number"""
+        self.next_number += 1
+        return self.next_number
+
+    def reset(self):
+        """Reset the current sequence number to original start number"""
+        self.next_number = self.start - 1
